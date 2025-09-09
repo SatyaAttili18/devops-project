4b05f35d2679a250894aae4ec5930a5e0e86194b
SatyaAttili18 <111959661+SatyaAttili18@users.noreply.github.com>
2025-09-09 09:13:07 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Jahnavi - DevOps Project!\n');
+  res.end('Hello from Satya - DevOps Project!\n');
 });
 
 server.listen(port, () => {
